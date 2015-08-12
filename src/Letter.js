9ecb871424349323f2c6548fbc09b657cd5c034b
idglv <idglv@outlook.com>
2015-08-12 19:56:25 +0300

    letter style

--- src/Letter.js
+++ src/Letter.js
@@ -5,10 +5,10 @@ export default class Letter extends Component {
     let className = "letter";
     let fnClick = this.props.onClick;
     if (this.props.enabled)  {
-      className += " letter--enabled";
+      className += " letter_enabled";
       fnClick = null;
     } else {
-      className += " letter--active";
+      className += " letter_active";
     }
     return (
       <a className={className} onClick={fnClick}>{this.props.letter}</a>
