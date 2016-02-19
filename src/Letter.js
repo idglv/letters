83ffc9be004d722c0ce9764ab6e0dadd1a80aefe
idglv <idglv@outlook.com>
2016-02-19 22:46:44 +0300

    classnames

--- src/Letter.js
+++ src/Letter.js
@@ -1,22 +1,30 @@
 import React, { Component } from 'react';
 
-export default class Letter extends Component {
-  render() {
-    let className = 'letter';
-    let fnClick = this.props.onClick;
-    if (!this.props.enabled) {
-      className += ' letter_enabled';
-      fnClick = null;
-    } else {
-      className += ' letter_active';
-    }
+import cx from 'classnames';
 
-    if (this.props.wrong) {
-      className += ' letter_wrong';
+export default class Letter extends Component {
+  handleClick = (event) => {
+    if (this.props.enabled && this.props.onClick) {
+      this.props.onClick(event);
     }
+  }
+
+  render() {
+    let props = this.props;
+
+    let letterClass = cx('letter', {
+      'letter_enabled': !props.enabled,
+      'letter_active': props.enabled,
+      'letter_wrong': props.wrong
+    })
 
     return (
-      <a className={className} onClick={fnClick}>{this.props.letter.toUpperCase()}</a>
+      <a
+        className={letterClass}
+        onClick={this.handleClick}
+      >
+        {props.letter.toUpperCase()}
+      </a>
     );
   }
 }
